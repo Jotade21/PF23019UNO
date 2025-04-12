13d0133b6c6b652254e96c9f44126fe8f94ab5f1
PF23019UNO <pf23019@ues.edu.sv>
2025-04-12 15:02:57 -0600

    Agregue nuevos archivos usando Pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Josue David Parada Flores",
     author_email="PF23019@ues.edu.sv",
     description="Librería para resolver sistemas lineales y no lineales",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Jotade21/PF23019UNO.git",
     packages= ['PF23019UNO'],
